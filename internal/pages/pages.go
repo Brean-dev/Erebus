99424468c6e1903e26a83a853736676762c5a8ef
Brean-dev <personal@breandan.nl>
2026-02-12 18:07:04 +0100

    fix(pages.go): Restore and enhance bot detection logic in LogRequest

--- internal/pages/pages.go
+++ internal/pages/pages.go
@@ -102,41 +102,46 @@ func GenerateHandler(w http.ResponseWriter, r *http.Request) {
 
 func LogRequest(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// skip noisy automatic requests
-		if r.URL.Path == "/favicon.ico" || r.URL.Path == "/robots.txt" {
-			handler.ServeHTTP(w, r)
-			return
-		}
-
 		ua := r.Header.Get("User-Agent")
 		// secChUa := r.Header.Get("Sec-CH-UA")
 		accept := r.Header.Get("Accept")
 		lang := r.Header.Get("Accept-Language")
 		encoding := r.Header.Get("Accept-Encoding")
 		cfConnectingIP := r.Header.Get("CF-Connecting-IP")
+		
+		// skip noisy automatic requests
+		if r.URL.Path == "/favicon.ico" || r.URL.Path == "/robots.txt" {
+			handler.ServeHTTP(w, r)
+			return
+		}
 
-		// // --- simple scraper detection ---
-		// isBot := false
-		// lowerUA := strings.ToLower(ua)
-		// if ua == "" ||
-		// 	strings.Contains(lowerUA, "curl") ||
-		// 	strings.Contains(lowerUA, "python") ||
-		// 	strings.Contains(lowerUA, "scrapy") ||
-		// 	strings.Contains(lowerUA, "httpclient") ||
-		// 	strings.Contains(lowerUA, "go-http-client") ||
-		// 	strings.Contains(lowerUA, "httpx") ||
-		// 	strings.Contains(lowerUA, "bot") ||
-		// 	secChUa == "" ||
-		// 	!strings.HasPrefix(ua, "Mozilla/5.0") ||
-		// 	len(r.Header) < 5 {
-		// 	isBot = true
-		// }
-		//
-		// tag := ""
-		// if isBot {
-		// 	tag = "[BOT]"
-		// }
-		//
+		
+
+		if ua == "Wget" {
+			return
+		}
+		// --- simple scraper detection ---
+		isBot := false
+		lowerUA := strings.ToLower(ua)
+		if ua == "" ||
+			strings.Contains(lowerUA, "curl") ||
+			strings.Contains(lowerUA, "python") ||
+			strings.Contains(lowerUA, "scrapy") ||
+			strings.Contains(lowerUA, "httpclient") ||
+			strings.Contains(lowerUA, "go-http-client") ||
+			strings.Contains(lowerUA, "httpx") ||
+			strings.Contains(lowerUA, "bot") ||
+			secChUa == "" ||
+			!strings.HasPrefix(ua, "Mozilla/5.0") ||
+			len(r.Header) < 5 {
+			isBot = true
+		}
+		
+		tag := ""
+		if isBot {
+			tag = "[BOT]"
+		}
+		
 		log.Printf("%s %s %s %s UA=%q Accept=%q Lang=%q Enc=%q Headers=%d CF-Connecting-IP=%s",
 			r.RemoteAddr,
 			r.Method,
