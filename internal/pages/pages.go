476727624a4a81eb29031096f9fbad605a996ae6
Brean-dev <personal@breandan.nl>
2026-02-14 14:53:17 +0100

    refactor(pages.go): added the logger to requests

--- internal/pages/pages.go
+++ internal/pages/pages.go
@@ -6,19 +6,11 @@ import (
 	"html"
 	"math/rand"
 	"net/http"
-	"os"
 	"strings"
 	"time"
 
 	"Erebus/internal/bable"
-	"Erebus/internal/logger"
 )
-
-var log logger.Logger
-
-func init() {
-	log = logger.NewStandardLogger(os.Stdout, logger.InfoLevel)
-}
 
 //	func loggingMiddleware(next http.HandlerFunc) http.HandlerFunc {
 //		return func(w http.ResponseWriter, r *http.Request) {
@@ -128,77 +120,4 @@ func GenerateHandler(w http.ResponseWriter, r *http.Request) {
     </ul>
 </body></html>`)
 	flusher.Flush()
-}
-
-func LogRequest(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-		ua := r.Header.Get("User-Agent")
-		secChUa := r.Header.Get("Sec-CH-UA")
-		accept := r.Header.Get("Accept")
-		lang := r.Header.Get("Accept-Language")
-		encoding := r.Header.Get("Accept-Encoding")
-		cfConnectingIP := r.Header.Get("CF-Connecting-IP")
-
-		// skip noisy automatic requests
-		if r.URL.Path == "/favicon.ico" || r.URL.Path == "/robots.txt" {
-			handler.ServeHTTP(w, r)
-			return
-		}
-
-		if ua == "Wget" {
-			return
-		}
-		// --- simple scraper detection ---
-		isBot := false
-		lowerUA := strings.ToLower(ua)
-		if ua == "" ||
-			strings.Contains(lowerUA, "curl") ||
-			strings.Contains(lowerUA, "python") ||
-			strings.Contains(lowerUA, "scrapy") ||
-			strings.Contains(lowerUA, "httpclient") ||
-			strings.Contains(lowerUA, "go-http-client") ||
-			strings.Contains(lowerUA, "httpx") ||
-			strings.Contains(lowerUA, "bot") ||
-			secChUa == "" ||
-			!strings.HasPrefix(ua, "Mozilla/5.0") ||
-			len(r.Header) < 5 {
-			isBot = true
-		}
-
-		tag := ""
-		if isBot {
-			tag = "[BOT]"
-		}
-		reqLog := log.WithFields(
-			logger.Field{Key: "tag", Value: tag},
-			logger.Field{Key: "remote_addr", Value: r.RemoteAddr},
-			logger.Field{Key: "method", Value: r.Method},
-			logger.Field{Key: "remote_path", Value: r.URL.Path},
-			logger.Field{Key: "proto", Value: r.Proto},
-			logger.Field{Key: "user_agent", Value: ua},
-			logger.Field{Key: "accept", Value: accept},
-			logger.Field{Key: "lang", Value: lang},
-			logger.Field{Key: "encoding", Value: encoding},
-			logger.Field{Key: "header_len", Value: len(r.Header)},
-			logger.Field{Key: "real_ip", Value: cfConnectingIP},
-		)
-		reqLog.Info(ctx, "serving request")
-
-		// log.Printf("%s %s %s %s %s UA=%q Accept=%q Lang=%q Enc=%q Headers=%d CF-Connecting-IP=%s",
-		// 	tag,
-		// 	r.RemoteAddr,
-		// 	r.Method,
-		// 	r.URL.Path,
-		// 	r.Proto,
-		// 	ua,
-		// 	accept,
-		// 	lang,
-		// 	encoding,
-		// 	len(r.Header),
-		// 	cfConnectingIP,
-		// )
-		//
-		handler.ServeHTTP(w, r)
-	})
 }
