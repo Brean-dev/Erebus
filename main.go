f7c4e5f4df2fff389873c5a768372d57ac27e0a4
Brean-dev <personal@breandan.nl>
2026-02-15 00:13:12 +0100

    refactor(main): code formatting

--- main.go
+++ main.go
@@ -15,13 +15,15 @@ import (
 func main() {
 	httpPort := 8080
 
-	slog.SetDefault(slog.New(slogcolor.NewHandler(os.Stderr, slogcolor.DefaultOptions)))
+	slog.SetDefault(slog.New(slogcolor.NewHandler(os.Stderr,
+		slogcolor.DefaultOptions)))
 	// Use a catch-all handler that responds to all endpoints
 	http.HandleFunc("/", pages.GenerateHandler)
 
 	slog.Info("server started on :8080")
 
-	err := http.ListenAndServe(fmt.Sprintf(":%d", httpPort), utils.LogRequest(http.DefaultServeMux))
+	err := http.ListenAndServe(fmt.Sprintf(":%d", httpPort),
+		utils.LogRequest(http.DefaultServeMux))
 	if err != nil {
 		slog.Error(err.Error())
 	}
