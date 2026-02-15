76784f9d9f33fdfd585d02a9834502d8be7f7f4d
Brean-dev <personal@breandan.nl>
2026-02-15 16:14:34 +0100

    chore(bable): removed some comments and formatting:w

--- internal/bable/bable.go
+++ internal/bable/bable.go
@@ -97,7 +97,8 @@ func tokenize(text string) []string {
 
 func isPunctuation(s string) bool {
 	for _, r := range s {
-		if (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') || (r >= '0' && r <= '9') || r == '\'' {
+		if (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') ||
+			(r >= '0' && r <= '9') || r == '\'' {
 			return false
 		}
 	}
@@ -222,7 +223,6 @@ func Bable(numSentences int, prefixLen int) string {
 	c := NewChain(prefixLen)
 	c.Build(ReadManifesto())
 
-	// Use the new sentence generator
 	text := c.GenerateSentences(numSentences)
 
 	return text
