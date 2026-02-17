3230e5cff71edd659a3c382e06825f4acc7926e8
Brean-dev <personal@breandan.nl>
2026-02-17 11:55:04 +0100

    fix(rediscache): changed localhost to REDIS_HOST

--- internal/rediscache/connection.go
+++ internal/rediscache/connection.go
@@ -19,7 +19,7 @@ type RedisClient struct {
 // NewRedisClient creates a new struct RedisClient.
 func NewRedisClient() (*RedisClient, error) {
 	rdb := redis.NewClient(&redis.Options{
-		Addr: fmt.Sprintf("%s:%s", "localhost",
+		Addr: fmt.Sprintf("%s:%s", os.Getenv("REDIS_HOST"),
 			os.Getenv("REDIS_PORT")),
 		Password: os.Getenv("REDIS_PASSWORD"),
 		DB:       0,
